ec2698c8b53a9e0ed9ca89342ab604410b7a5191
Andy Dobbs <andrew.dobbs@gmail.com>
2023-12-31 09:04:08 -0500

    Initial Commit

---
+++ BookShelf/BookShelfApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookShelfApp.swift
+//  BookShelf
+//
+//  Created by Andy Dobbs on 12/31/23.
+//
+
+import SwiftUI
+
+@main
+struct BookShelfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
